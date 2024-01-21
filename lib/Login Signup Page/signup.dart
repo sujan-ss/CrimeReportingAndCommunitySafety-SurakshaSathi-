e8df61f8882e543cd995c494dbda6c23bffef419
unknown <ss.shresthasujan@gmail.com>
2024-01-21 14:09:12 +0545

    Error in the SignUp page, Showing the validation text without user input and Validation needs to be completed

--- lib/Login Signup Page/signup.dart
+++ lib/Login Signup Page/signup.dart
@@ -6,28 +6,37 @@ import 'package:image_picker/image_picker.dart';
 import 'package:suraksha_saathi/Login%20Signup%20Page/login_signin_screen.dart';
 import 'package:suraksha_saathi/Login%20Signup%20Page/signup_email_screen.dart';
 
-class SignUpPage extends StatelessWidget {
-  const SignUpPage({super.key});
+class SignUpPage extends StatefulWidget {
+  const SignUpPage({Key? key}) : super(key: key);
+
+  @override
+  State<SignUpPage> createState() => _SignUpPage();
+}
+
+class _SignUpPage extends State<SignUpPage> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController fNameController = TextEditingController();
+  final TextEditingController lNameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  File? _photoImage;
+  File? _passportImage;
+
+  Future<void> _getImage(bool isPassport) async {
+    final ImagePicker _picker = ImagePicker();
+    final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+
+    if (image != null) {
+      final File file = File(image.path);
+      if (isPassport) {
+        _passportImage = file;
+      } else {
+        _photoImage = file;
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    File? _photoImage;
-    File? _passportImage;
-
-    Future<void> _getImage(bool isPassport) async {
-      final ImagePicker _picker = ImagePicker();
-      final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
-
-      if (image != null) {
-        final File file = File(image.path);
-        if (isPassport) {
-          _passportImage = file;
-        } else {
-          _photoImage = file;
-        }
-      }
-    }
-
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
@@ -43,218 +52,244 @@ class SignUpPage extends StatelessWidget {
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Create Your Account',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 20),
-              Text(
-                'First Name',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              TextFormField(
-                // Add your logic for handling first name input
-                decoration: InputDecoration(
-                  hintText: 'Enter your first name',
-                ),
-              ),
-              SizedBox(height: 20),
-              Text(
-                'Last Name',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              TextFormField(
-                // Add your logic for handling last name input
-                decoration: InputDecoration(
-                  hintText: 'Enter your last name',
-                ),
-              ),
-              SizedBox(height: 20),
-              Text(
-                'Email',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              TextFormField(
-                // Add your logic for handling email input
-                decoration: InputDecoration(
-                  hintText: 'Enter your email',
-                ),
-              ),
-              SizedBox(height: 20),
-              Divider(),
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 10),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Country',
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          DropdownButton<String>(
-                            items: [
-                              DropdownMenuItem(
-                                  value: '+977', child: Text('Nepal')),
-                            ],
-                            onChanged: (value) {
-                              // Handle country selection
-                            },
-                            value: '+977',
-                          ),
-                        ],
-                      ),
-                    ),
-                    VerticalDivider(),
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Phone Number',
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          TextFormField(
-                            decoration: InputDecoration(
-                              hintText: 'Enter your phone number',
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(height: 20),
-              Text(
-                'Upload Photo',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 10),
-              GestureDetector(
-                onTap: () async {
-                  await _getImage(false);
-                },
-                child: _photoImage == null
-                    ? Container(
-                        width: 100,
-                        height: 100,
-                        color: Colors.grey,
-                        child: Icon(Icons.camera_alt, color: Colors.white),
-                      )
-                    : Image.file(
-                        _photoImage!,
-                        width: 100,
-                        height: 100,
-                        fit: BoxFit.cover,
-                      ),
-              ),
-              SizedBox(height: 20),
-              Text(
-                'Passport',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 10),
-              GestureDetector(
-                onTap: () async {
-                  await _getImage(true);
-                },
-                child: _passportImage == null
-                    ? Container(
-                        width: 100,
-                        height: 100,
-                        color: Colors.grey,
-                        child: Icon(Icons.camera_alt, color: Colors.white),
-                      )
-                    : Image.file(
-                        _passportImage!,
-                        width: 100,
-                        height: 100,
-                        fit: BoxFit.cover,
-                      ),
-              ),
-              SizedBox(height: 20),
-              Text(
-                'Password',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              TextFormField(
-                obscureText: true,
-                onChanged: (value) {},
-                decoration: InputDecoration(
-                  hintText: 'Enter your password',
-                ),
-              ),
-              SizedBox(height: 20),
-              Text(
-                'Confirm Password',
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              TextFormField(
-                obscureText: true,
-                onChanged: (value) {},
-                decoration: InputDecoration(
-                  hintText: 'Confirm your password',
-                ),
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  // Handle create new account button press
-                  print('SignUp pressed');
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => SignUpEmailScreen()),
-                  );
-                },
-                style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0),
+          child: Form(
+            key: _formKey,
+            autovalidateMode:
+                AutovalidateMode.always, // Set autovalidateMode to always
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Create Your Account',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
                   ),
-                  backgroundColor: Color(0xFF32508E),
-                  foregroundColor: Colors.white,
-                  padding:
-                      EdgeInsets.symmetric(horizontal: 30.0, vertical: 15.0),
                 ),
-                child: Text('SignUp'),
-              ),
-            ],
+                SizedBox(height: 20),
+                TextFormField(
+                  keyboardType: TextInputType.text,
+                  controller: fNameController,
+                  decoration: InputDecoration(
+                    labelText: "First Name",
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.person),
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "Enter First Name";
+                    } else if (fNameController.text == lNameController.text) {
+                      return 'First name and last name cannot be the same.';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 20),
+                TextFormField(
+                  keyboardType: TextInputType.text,
+                  controller: lNameController,
+                  decoration: InputDecoration(
+                    labelText: "Last Name",
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.person),
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "Enter Last Name";
+                    } else if (fNameController.text == lNameController.text) {
+                      return 'First name and last name cannot be the same.';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 20),
+                TextFormField(
+                  keyboardType: TextInputType.emailAddress,
+                  controller: emailController,
+                  decoration: InputDecoration(
+                    labelText: "Email",
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.email),
+                  ),
+                  validator: (value) {
+                    bool emailValid = RegExp(
+                            r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+/=?^_`{|}~]+@[a-zA-Z0-9+\.[a-zA-Z]+")
+                        .hasMatch(value!);
+
+                    if (value.isEmpty) {
+                      return "Enter Email";
+                    } else if (!emailValid) {
+                      return "Enter Valid Email";
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 20),
+                Divider(),
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 10),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Country',
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            DropdownButton<String>(
+                              items: [
+                                DropdownMenuItem(
+                                  value: '+977',
+                                  child: Text('Nepal'),
+                                ),
+                              ],
+                              onChanged: (value) {
+                                // Handle country selection
+                              },
+                              value: '+977',
+                            ),
+                          ],
+                        ),
+                      ),
+                      VerticalDivider(),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Phone Number',
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            TextFormField(
+                              decoration: InputDecoration(
+                                hintText: 'Enter your phone number',
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  'Upload Photo',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 10),
+                GestureDetector(
+                  onTap: () async {
+                    await _getImage(false);
+                  },
+                  child: _photoImage == null
+                      ? Container(
+                          width: 100,
+                          height: 100,
+                          color: Colors.grey,
+                          child: Icon(Icons.camera_alt, color: Colors.white),
+                        )
+                      : Image.file(
+                          _photoImage!,
+                          width: 100,
+                          height: 100,
+                          fit: BoxFit.cover,
+                        ),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  'Passport',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 10),
+                GestureDetector(
+                  onTap: () async {
+                    await _getImage(true);
+                  },
+                  child: _passportImage == null
+                      ? Container(
+                          width: 100,
+                          height: 100,
+                          color: Colors.grey,
+                          child: Icon(Icons.camera_alt, color: Colors.white),
+                        )
+                      : Image.file(
+                          _passportImage!,
+                          width: 100,
+                          height: 100,
+                          fit: BoxFit.cover,
+                        ),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  'Password',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  onChanged: (value) {},
+                  decoration: InputDecoration(
+                    hintText: 'Enter your password',
+                  ),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  'Confirm Password',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  onChanged: (value) {},
+                  decoration: InputDecoration(
+                    hintText: 'Confirm your password',
+                  ),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    // Validate the form
+                    if (_formKey.currentState?.validate() ?? false) {
+                      // Proceed if validation succeeds
+                      print('SignUp pressed');
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => SignUpEmailScreen()),
+                      );
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    backgroundColor: Color(0xFF32508E),
+                    foregroundColor: Colors.white,
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 30.0, vertical: 15.0),
+                  ),
+                  child: Text('SignUp'),
+                ),
+              ],
+            ),
           ),
         ),
       ),
